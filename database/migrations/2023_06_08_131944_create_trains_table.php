9c3226dcba51d56578e756f6ff7d1f6687770e4f
MicheleBertoia2 <michelebertoia95@gmail.com>
2023-06-08 15:37:56 +0200

    [modify] migration file updated with all column needed

--- database/migrations/2023_06_08_131944_create_trains_table.php
+++ database/migrations/2023_06_08_131944_create_trains_table.php
@@ -9,12 +9,23 @@ return new class extends Migration
     /**
      * Run the migrations.
      *
+
+     *
      * @return void
      */
     public function up()
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('azienda');
+            $table->string('stazione_di_partenza');
+            $table->string('stazione_di_arrivo');
+            $table->time('orario_di_partenza');
+            $table->time('orario_di_arrivo');
+            $table->string('codice_treno')->unique();
+            $table->tinyInteger('numero_carrozze')->unsigned();
+            $table->boolean('in_orario')->default('1');
+            $table->boolean('cancellato')->default('0');
             $table->timestamps();
         });
     }
@@ -29,3 +40,13 @@ return new class extends Migration
         Schema::dropIfExists('trains');
     }
 };
+
+// Azienda
+// Stazione di partenza
+// Stazione di arrivo
+// Orario di partenza
+// Orario di arrivo
+// Codice Treno
+// Numero Carrozze
+// In orario
+// Cancellato
